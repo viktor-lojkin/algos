b352ed1eb464d39bcfc52e0b8b739b4e312634df
Viktor Lozhkin <122951966+viktor-lojkin@users.noreply.github.com>
2024-02-10 11:51:47 +0300

    Update sliding_window.h

--- problem_solving_patterns/sliding_window.h
+++ problem_solving_patterns/sliding_window.h
@@ -2,19 +2,19 @@
 #include <algorithm>
 #include <stdexcept>
 
-int FindMaxSubarray(std::vector<int> input, int length_of_subarray) {    // i'm gonna improve it with comparator
+int FindMaxSubarray(std::vector<int> input, size_t length_of_subarray) {   // i'm gonna improve it with comparator
     if(input.size() < length_of_subarray) {
-        throw std::logic_exception("The array is smaller than subarray!");
+        throw std::logic_error("The array is smaller than subarray!");
     }
 
     int max_value = 0;
     int cur_value = 0;
 
-    for (int i = 0; i < input.size(); ++i) {
+    for (size_t i = 0; i < input.size(); ++i) {
         cur_value += input[i];
-        if(i >= length_of_subarray - 1) {                    // (length_of_subarray - 1) is index from which we can check our window
+        if(i >= length_of_subarray - 1u) {
             max_value = std::max(max_value, cur_value);
-            cur_value -= input[i - (length_of_subarray - 1)];
+            cur_value -= input[i - (length_of_subarray - 1)];   // (length_of_subarray - 1) is index from which we can check our window
         }
     }
 
