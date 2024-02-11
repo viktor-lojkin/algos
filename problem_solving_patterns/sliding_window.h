0ab582066f71d2f30c004e0570d05bddc5e13d8d
Viktor Lozhkin <122951966+viktor-lojkin@users.noreply.github.com>
2024-02-11 11:35:59 +0300

    templated for iterators, support negative and positive numbers

--- problem_solving_patterns/sliding_window.h
+++ problem_solving_patterns/sliding_window.h
@@ -1,20 +1,28 @@
-#include <vector>
+#include <iterator>
 #include <algorithm>
 #include <stdexcept>
 
-int FindMaxSubarray(std::vector<int> input, size_t length_of_subarray) {   // i'm gonna improve it with comparator
-    if(input.size() < length_of_subarray) {
+
+template <typename It>
+int FindMaxSubarray(It begin, It end, size_t length_of_subarray) { 
+    size_t size = std::distance(begin, end);
+    if(size < length_of_subarray) {
         throw std::logic_error("The array is smaller than subarray!");
     }
 
-    int max_value = 0;
-    int cur_value = 0;
+    bool is_updtd {};
+    int max_value {};
+    int cur_value {};
 
-    for (size_t i = 0; i < input.size(); ++i) {
-        cur_value += input[i];
-        if(i >= length_of_subarray - 1u) {
+    for (It it {begin}; it < end; ++it) {
+        cur_value += *it;
+        if(std::distance(begin, it) >= length_of_subarray - 1u) {
+            if (!is_updtd) {                                       // when we first enter the if-statement above 
+                max_value = cur_value;                             // we need to update max_value with valid value 
+                is_updtd = true;                                   // update flag to make it once
+            }
             max_value = std::max(max_value, cur_value);
-            cur_value -= input[i - (length_of_subarray - 1)];   // (length_of_subarray - 1) is index from which we can check our window
+            cur_value -= *(it - (length_of_subarray - 1));
         }
     }
 
